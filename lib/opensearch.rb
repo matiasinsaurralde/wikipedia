84ade7c8be726cc8a42552f9d9cfbce8cb5ebdfb
Matias Insaurralde <matiasbaruchinsaurralde@gmail.com>
2013-05-04 16:49:56 -0400

    a different file for opensearch

---
+++ lib/opensearch.rb
@@ -0,0 +1,11 @@
+# encoding: utf-8
+
+module Wikipedia
+
+	## documentation: http://www.mediawiki.org/wiki/API%3aOpensearch
+	## ex.: http://en.wikipedia.org/w/api.php?action=opensearch&search=api&limit=10&namespace=0&format=jsonfm
+
+	module OpenSearch
+	end
+
+end
