13ce36d51c5e1251099321f34f29f6aa9d5b16a7
Matias Insaurralde <matiasbaruchinsaurralde@gmail.com>
2013-05-03 21:17:48 -0400

    initial commit

---
+++ example.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+require './lib/wikipedia'
+
+require 'pp'
+
+pp Wikipedia::article 'Pareidolia', 'es'
---
+++ lib/wikipedia.rb
@@ -0,0 +1,34 @@
+# encoding: utf-8
+
+require 'rubygems'
+require 'bundler/setup'
+
+require 'open-uri'
+require 'htmlentities'
+require 'hpricot'
+
+module Wikipedia
+
+	URL = "http://%LANG%.wikipedia.org/w/api.php?action=parse&page="
+
+	def self.article( n, lang = 'es' )
+
+		texts = []
+
+		raw_data = open( URL.gsub("%LANG%", lang)+n ).read()
+		#raw_data = File.read('pareidolia').gsub("\n", "")
+
+		he = HTMLEntities.new()
+
+		# characters = { Regexp.new("\\[(.*)\\]") => '' }
+
+ 		raw_data = he.decode( he.decode( raw_data ) ).gsub("\n", "") # >:D
+
+		Hpricot(raw_data).search('p').each do |ph|
+			texts << ph.inner_text
+		end
+
+		return texts
+
+	end
+end
