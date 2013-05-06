e2f4cd71a3143b4b5f3122b32c68b4d06634af6e
Matias Insaurralde <matiasbaruchinsaurralde@gmail.com>
2013-05-06 05:38:16 -0400

    integrating some stuff from 'language'

--- lib/ambiguity.rb
+++ lib/ambiguity.rb
@@ -1,6 +1,107 @@
 # encoding: utf-8
 
+module Maths
+
+	def self.extract_terms( text )
+
+		terms = {}
+
+		all_terms = text.split(' ')
+
+		all_terms.each do |term|
+			if !terms[ term ]
+				terms.store( term, 0 )
+			end
+
+			terms[ term ] += 1
+		end
+
+		terms.percentages!( all_terms.size ).most_frequent
+
+	end
+
+	def self.compute_distance_between( text1, text2 )
+		text2.each do |k, v|
+			if !text1.include?(k)
+				text2.delete(k)
+			end
+		end
+		text1.each do |k, v|
+			if !text2[k]
+				text2.store(k, 0.0)
+			end
+		end
+		dot_product = dotp( text1, text2 )
+		magnitudes = [ magnitude( text1 ), magnitude( text2 ) ]
+		magnitude_p = magnitude_product( magnitudes[0], magnitudes[1] )
+		return similarity( dot_product, magnitude_p )
+	end
+
+	def self.dotp(v1, v2)
+
+		dot_product = []
+
+		v1.values.zip(v2.values).each do |p|
+
+			dot_product << ( p[0]*p[1] )
+
+		end
+
+		dot_product.inject(:+)
+
+	end
+
+	def self.magnitude(v)
+
+		m = []
+
+		v.values.each do |n|
+
+			m << n**2
+
+		end
+
+		return m.inject(:+)
+
+	end
+
+	def self.magnitude_product(m1, m2)
+		return Math.sqrt( m1 * m2 )
+	end
+
+	def self.similarity(dotp, magnitude_p)
+		n = (dotp.to_f / magnitude_p.to_f)*100.0
+		return n.round(2)
+	end
+
+end
+
 module Wikipedia
+
+	def self.do_classification( given_text, articles )
+		puts "do_classification()"
+
+		texts, terms, given_text_term_data = {}, {}, Maths::extract_terms( given_text )
+
+		articles.each do |article_name, article|
+			texts.store( article_name, article.texts.join('') )
+		end
+
+		texts.each do |article_name, text|
+
+			terms.store( article_name, Maths::extract_terms( text ) )
+
+		end
+
+		terms.each do |article_name, term_data|
+			puts "given_text -> #{article_name}"
+			p Maths::compute_distance_between( given_text_term_data, term_data )
+			puts
+		end
+
+
+		return 0
+	end
 
 	def self.disambiguate( given_text, ambiguous_article )
 
@@ -44,15 +145,15 @@ module Wikipedia
 
 		# get articles
 		section_articles.each do |section, articles|
-			puts "*** #{section}"
+			#puts "*** #{section}"
 			articles.each do |article_name, data|
-				puts "#{article_name}: \"#{data[:phrase]}\""
-				a = Wikipedia::article(article_name.dup)
-				puts a.texts
-				puts
+				#puts "#{article_name}: \"#{data[:phrase]}\""
+				other_articles.store( article_name, Wikipedia::article(article_name.dup) )
 			end
-			puts
+			#puts
 		end
+
+		do_classification( given_text, other_articles )
 
 	end
 
